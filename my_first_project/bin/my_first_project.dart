76e277d3480f923cf53a7ad03bee28595e78d90d
Puja Saha <nilasarkar@outlook.com>
2024-02-18 23:28:26 +0600

    Initial Commit

---
+++ my_first_project/bin/my_first_project.dart
@@ -0,0 +1,22 @@
+  /*List problems
+Take a list, say for example this one:
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+and write a program that prints out all the elements of the list that are less than 5.
+*/
+ void main(){
+  List<int> num=[1,1,2,3,5,8,13,21,3,6,8,34,55];
+  //num.sort();
+  print(num..sort());
+  for(int i=0;i<5;i++){
+    print(i);
+  }
+
+
+
+  String name = "pujasaha";
+  print(name.length);
+  print(name.toUpperCase());
+}
+
+
---
+++ my_first_project/test/my_first_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_first_project/my_first_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
